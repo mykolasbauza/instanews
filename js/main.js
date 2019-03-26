5904f353fe62d028ce2eea6410373a3c59ddc5dc
mykolasbauza <mykolas.bauza@gmail.com>
2019-03-26 12:57:40 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    const $loader = $('.loader');
+    $('select').on('change', function () {
+        $loader.css('display', 'flex');
+        $('footer').css('top', '1360px');
+        let url = 'https://api.nytimes.com/svc/topstories/v2/' + $('select').val() + '.json';
+        url += '?' + $.param({
+            'api-key': 'Z4Q0RWspjL4d7O2pBfSjdkmRjJZp7WNF'
+        });
+        $('.logo').addClass('shrink');
+        $('#idListe').addClass('form-anim');
+        $('body').addClass('body-move');
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (data) {
+            const $allArticle = $('.all-article');
+            $allArticle.empty();
+            let articleCounter = 0;
+            for (let i = 0; i < 12 && articleCounter < 12; i++) {
+                const img = data.results[i].multimedia[4].url;
+                const url = data.results[i].url;
+                const description = data.results[i].abstract;
+                if (data.results[i].multimedia.length) {
+                    articleCounter += 1;
+                    const $article = $('<a target="_blank" href="' +
+                        url + '"class="articleLink"><section class="articleSection"><h2 class="articleDescription">' +
+                        description + '</h2></section></a>');
+                    $allArticle.append($article) +
+                        $article.css('background-image', 'url("' + img + '")');
+                }
+                $loader.css('display', 'none');
+            }
+        }).fail(function (err) {
+            throw err;
+        });
+    })
+});
